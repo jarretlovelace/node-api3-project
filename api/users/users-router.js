7e8f6f68777b3633e688b805d5ea1f711a4de5de
Jarret Lovelace <jarretlovelace@icloud.com>
2024-08-11 03:13:41 -0600

    WIP

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const {
+  validateUserId,
+  validateUser,
+  validatePost,
+} = require('../middleware/middleware')
+
 
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
@@ -12,11 +18,13 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  console.log(req.user)
 });
 
 router.post('/', (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  console.log(req.name)
 });
 
 router.put('/:id', (req, res) => {
@@ -33,12 +41,23 @@ router.delete('/:id', (req, res) => {
 router.get('/:id/posts', (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  console.log(req.user)
 });
 
 router.post('/:id/posts', (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  console.log(req.user)
+  console.log(req.text)
 });
 
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    customMessage: 'a message',
+    message: err.message,
+    stack: err.stack,
+  })
+})
 // do not forget to export the router
+module.exports = router
